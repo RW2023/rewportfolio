e3e016c3aa5b4bbe6ae9d376645248ba165b00c8
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-21 21:16:22 -0400

    move construction message from app.js move into newly constructed header section in main html document. I know i am supposed to likely put a state dependent object there or something of the like. however I don't know how to link that yet. what i do know though is how to have it still render in the meantime which is progress!

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,13 +3,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className='App-header '>
-        <h1>👋Hi I am RE. Wilson</h1>
-        <h2>Welcome to My Portfolio </h2>
-        <h2>🔨 I am  Renovating and will be back soon 📏</h2>
-        <p> Stay Tuned for Updates 📢</p>
-        </header>
-     
+      
     </div>
   );
 }
