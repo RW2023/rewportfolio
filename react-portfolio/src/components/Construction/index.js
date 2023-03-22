3f5c642536b52c835c2763a6c1187fd8234b0315
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-03-22 16:49:44 -0400

    start building Construction component just the function for the jsx is complete at this point

---
+++ react-portfolio/src/components/Construction/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useEffect, useState } from 'react'
+import {
+    faAngular,
+    faCss3,
+    faGitAlt,
+    faHtml5,
+    faJsSquare,
+    faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+
+function Construction() {
+    return (
+        <div>
+            <h1>👋Hi I am RE. Wilson</h1>
+            <h2>Welcome to My Portfolio </h2>
+            <h2>🔨 I am Renovating and will be back soon 📏</h2>
+            <p>Stay Tuned for Updates 📢</p>
+        </div>
+    );
+}
+
+export default Construction;
